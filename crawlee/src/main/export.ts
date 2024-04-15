063d9dedb9adb4ba3ea292f8674f8d10d5644318
ericstefano <ericstefano12@gmail.com>
2024-04-14 22:22:15 -0300

    refactor: updated export

--- crawlee/src/main/export.ts
+++ crawlee/src/main/export.ts
@@ -1,8 +1,10 @@
 import { Dataset } from "crawlee";
 export const dataset = await Dataset.open("stackoverflow");
-const data = await dataset.getData();
+const info = await dataset.getInfo();
+const count = info?.itemCount;
+console.log(count);
 const fileName = "stackoverflow";
-if (data.count > 0) {
-  dataset.exportToCSV(fileName);
+if (count && count > 0) {
+  dataset.exportToJSON(fileName);
   console.log(`Exported ${fileName} on storage/key_value_stores/default`);
 }
